7f3940936cee3ec47a3a578c4ec585dd582cb013
saurabh <sauravparmar878@gmail.com>
2026-08-12 23:30:48 +0530

    add user and video model, created encryption and decryption methods for password and methods for jwt access token and jwt refresh tokens

---
+++ src/models/user.model.js
@@ -0,0 +1,97 @@
+import { password } from "input";
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, // cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    refreshToken: {
+        type: String
+    }
+}, {
+        timestamps: true
+    }
+);
+
+// NOTE: We are writing normal function here and not arrow function as arrow function does not have access to "this" due to which we would not be able to refer to the current instances, so using normal function;
+
+// To encrypt the password just before saving it to database with the help of "pre" hook provided by mongoose and "bcrypt" - a js package; also encrypt and store only when its a new record or change in existing password like while update password or reset password; used "hash" method which takes two params, the password to be encrypted and "salt" i.e no. of rounds in encryption, e.g.: 10
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+// custom hook/method to decrypt the encrypted password and compare with the db password, so one password provided by user and another is db password which can be referred to this.password 
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+// method to generate jwt access token using jwt library
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// method to generate jwt refresh token using jwt library 
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
