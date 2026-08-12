fa2666c48b4690265395e6e218ea998f36320456
saurabh <sauravparmar878@gmail.com>
2026-08-13 00:21:47 +0530

    file upload on server using multer by creating a middleware, file upload on cloudinary by creating an utility function

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// cloudinary configurtions
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// method to upload file on cloudinary and get back the url from cloudinry in response to store in database
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+
+        // upload the file om cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        // file has been uploaded successfully
+        console.log('file is uploaded on cloudinary ', response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
